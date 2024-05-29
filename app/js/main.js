e465fb6cbd725a937a5f6f38d81180fe18636c8d
Denis <dr144371@gmail.com>
2024-05-30 00:27:06 +0300

    Add smooth scroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,32 @@
 const header = document.querySelector(".header");
+const menu = document.querySelector(".menu__list");
+
+const scrollToElem = (elem) => {
+    const rect = elem.getBoundingClientRect();
+    const headerHeight = header.offsetHeight;
+    const top = rect.top + scrollY - headerHeight;
+
+    scrollTo({
+        top,
+        behavior: "smooth"
+    })
+}
+
+menu.addEventListener("click", (event) => {
+    event.preventDefault();
+    const elem = event.target;
+    if (elem !== null && elem.classList.contains("menu__link")) {
+        const target = document.querySelector(elem.hash);
+        scrollToElem(target);
+    }
+})
+
+
 window.addEventListener("scroll", () => {
-    console.log(window.scrollY);
     if (window.scrollY > 450) {
         header.classList.add("header-fixed");
     } else {
         header.classList.remove("header-fixed");
     }
 })
+
